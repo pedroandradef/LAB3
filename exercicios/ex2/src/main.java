171b8f289633aeaf4e9dc7368349f3f687d4bca2
Pedro <pedroandradef@outlook.com>
2019-05-16 08:58:01 -0300

    ex1 PAA pronto

--- exercicios/ex2/src/main.java
+++ exercicios/ex2/src/main.java
@@ -1,31 +1,30 @@
 public class main {
-    public static void main(String [] args){
-        int lista[]= main.organizador(new int[]{9,8,7,6,5,3,2,1,4}, 0);
-        for(int i = 0; i < 9; i++)
+    public static void main(String[] args) {
+        int lista[] = main.organizador(new int[]{9, 8, 7, 6, 5, 3, 2, 1, 4, 1,9,8,7,5,4,10,588,599,955,10}, 0);
+        for (int i = 0; i < 20; i++)
             System.out.println(lista[i]);
     }
 
-    public static int[] organizador(int lista[], int indice){
+    public static int[] organizador(int lista[], int indice) {
 
-        if(lista.length > 0){
+        if (lista.length > 0) {
 
-        if (indice == lista.length -1 ){
-            return lista;}
-
-        int min = lista[indice];
-
-
-        for(int i = indice; i < lista.length; i++ ){
-            if(min > lista[i]){
-                min = lista[i];
-               int pos = i;
-                lista[pos] = lista[indice];
-                lista[indice] = min;
-                return organizador(lista,indice+1);
+            if (indice == lista.length - 1) {
+                return lista;
             }
 
-        }
-        return organizador(lista,indice+1);
+            int min = lista[indice];
+
+
+            for (int i = indice; i < lista.length; i++) {
+                if (min > lista[i]) {
+                    min = lista[i];
+                    int pos = i;
+                    lista[pos] = lista[indice];
+                    lista[indice] = min;
+                }
+            }
+            return organizador(lista, indice + 1);
         }
 
         throw new UnsupportedOperationException("Vazio");
