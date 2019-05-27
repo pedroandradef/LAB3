6d9761085c6d6996e3147a9f3fe4b72aed97835f
Pedro <pedroandradef@outlook.com>
2019-05-27 12:06:22 -0300

    limpando git

--- exercicios/ex2/src/main.java
+++ exercicios/ex2/src/main.java
@@ -1,8 +1,16 @@
+import java.util.Random;
 public class main {
     public static void main(String[] args) {
-        int lista[] = main.organizador(new int[]{9, 8, 7, 6, 5, 3, 2, 1, 4, 1,9,8,7,5,4,10,588,599,955,10}, 0);
-        for (int i = 0; i < 20; i++)
-            System.out.println(lista[i]);
+        Random rdm = new Random();
+        int vetor[] = new int [1000];
+        for (int i = 0; i< 1000; i++)
+            vetor[i] = rdm.nextInt();
+        long inicio = System.currentTimeMillis();
+        main.organizador(vetor,0);
+        System.out.println(System.currentTimeMillis() - inicio);
+        inicio = System.currentTimeMillis();
+        quickSort(vetor, 0, vetor.length - 1);
+        System.out.println(System.currentTimeMillis() - inicio);
     }
 
     public static int[] organizador(int lista[], int indice) {
@@ -26,7 +34,38 @@ public class main {
             }
             return organizador(lista, indice + 1);
         }
-
         throw new UnsupportedOperationException("Vazio");
+
+    }
+
+
+    public static void quickSort(int v[], int esquerda, int direita) {
+
+        int esq = esquerda;
+        int dir = direita;
+        int pivo = v[(esq + dir) / 2];
+        int troca;
+        while (esq <= dir) {
+            while (v[esq] < pivo) {
+                esq = esq + 1;
+            }
+            while (v[dir] > pivo) {
+                dir = dir - 1;
+            }
+            if (esq <= dir) {
+                troca = v[esq];
+                v[esq] = v[dir];
+                v[dir] = troca;
+                esq = esq + 1;
+                dir = dir - 1;
+            }
+        }
+        if (dir > esquerda)
+            quickSort(v, esquerda, dir);
+        if (esq < direita)
+            quickSort(v, esq, direita);
+
     }
 }
+
+
